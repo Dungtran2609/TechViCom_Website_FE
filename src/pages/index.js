84b439c84be4b01df9811e1295aeadde489a7a3a
H2369 <69509066+H2369@users.noreply.github.com>
2025-08-01 00:49:15 +0700

    sua loi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 // Export tất cả pages từ các thư mục con
-export * from './auth/index.js';
-export * from './user/index.js';
-export * from './product/index.js';
+export * from './auths/index.js';
+export * from './users/index.js';
+export * from './products/index.js';
 export * from './news/index.js';
-export * from './order/index.js';
-export * from './voucher/index.js';
-export * from './static/index.js';
-export * from './home/index.js'; 
+export * from './orders/index.js';
+export * from './vouchers/index.js';
+export * from './statics/index.js';
+export * from './homes/index.js'; 
